9978ecf6b4c747a0096765d0b59130526889bf02
Eason Yan <EasonYan@Easons-MacBook-Air.local>
2016-11-10 12:17:51 -0800

    1

--- Medium/src/PathsumII.java
+++ Medium/src/PathsumII.java
@@ -1,6 +1,27 @@
 
+import java.util.*;
 public class PathsumII {
-
+	public static List<List<Integer>> pathSum(TreeNode root, int sum){
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		List<Integer>subresult =  new ArrayList<Integer>();
+		pathSum(root,sum,result,subresult);
+		return result;
+				
+	}
+	public static void pathSum(TreeNode root, int sum,List<List<Integer>> result,List<Integer>subresult){
+		if(root==null) return ;
+		subresult.add(root.val);
+		if(root.left!=null){
+			pathSum(root.left,sum-root.val,result,subresult);
+		}
+		if(root.right!=null){
+			pathSum(root.right,sum-root.val,result,subresult);
+		}
+		if(root.left==null&&root.right==null&&sum==root.val){
+			result.add(new ArrayList(subresult));
+			subresult.remove(subresult.size()-1);
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
